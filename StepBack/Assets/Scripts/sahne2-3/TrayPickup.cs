6986704bad75135b1aac52a648f6aea36f79bab5
MeryemTuna1 <meryemsenatuna@gmail.com>
2026-01-21 23:53:37 +0300

    Eklemeler

--- StepBack/Assets/Scripts/sahne2-3/TrayPickup.cs
+++ StepBack/Assets/Scripts/sahne2-3/TrayPickup.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class TrayPickup : MonoBehaviour
 {
+    public AudioClip clip;
+
     void OnMouseDown()
     {
         TrayCarrySystem player = FindObjectOfType<TrayCarrySystem>();
         player.TakeTray(gameObject);
 
         GetComponent<Collider>().enabled = false;
+
+        KarakterIcSesManager.Instance.PlayInnerVoice(clip);
     }
 }
